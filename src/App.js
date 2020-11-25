cdef2f6edbe3aa0158bac416bc8841241279b7cc
Miguel Penades <miguelps63@gmail.com>
2020-11-25 15:10:49 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import * as React from "react";
+import { Admin, Resource, ListGuesser } from 'react-admin';
+import jsonServerProvider from 'ra-data-json-server';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const dataProvider = jsonServerProvider('https://my-json-server.typicode.com/MiguelSuay/hackathon-port-db');
+
+const App = () => (
+   <Admin dataProvider={dataProvider}>
+        <Resource name="sellers" list={ListGuesser} />
+        <Resource name="buyers" list={ListGuesser} />
+        <Resource name="sales_listings" list={ListGuesser} />
+        <Resource name="request_listings" list={ListGuesser} />
+        <Resource name="categorias" list={ListGuesser} />
+    </Admin>
+ );
 
 export default App;
